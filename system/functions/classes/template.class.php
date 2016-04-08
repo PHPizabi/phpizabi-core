20483c6fe54a33f11fc1167ea3d4a6fc6571ae30
Andy James <andy.james@rackspace.com>
2016-04-08 09:02:05 -0500

    starting working on pdo change (#1)
    start of pdo
    
    minor changes to templating
    
    removing main myQ calls
    
    Delete conf.inc.php
    
    missed one
    
    final theme fixes

--- system/functions/classes/template.class.php
+++ system/functions/classes/template.class.php
@@ -23,31 +23,31 @@
 
 	class template {
 		var $File; var $Buffer;
-		
+
 		// LOAD ////////////////////////////////////////////////////////////////////
 		function Load($file) {
 			global $CONF;
-			
+
 			/* Find out if we're using the strict mode or not */
 			if (substr($file,0,1) == "!") { /* Strict Mode (Prefixed by "!") */
 				$loadFile = substr($file,1);
 				$strict = true;
-			} 
-			
+			}
+
 			else { /* Not strict, use the ?L= get value to get the directory name */
 				if (isset($_GET["L"])) {
 					$loadFile = NULL;
 					for ($i=0; $i<count($dir=explode(".", $_GET["L"], 100))-1; $i++) {
 						$loadFile .= $dir[$i] . "/";
-					}					
+					}
 					$loadFile .= $file;
 					$strict = false;
-				} 
+				}
 				else die("Not strict template call outside routing");
 			}
-			
+
 			if (substr($loadFile, strlen($loadFile)-4) != ".tpl") $loadFile .= ".tpl";
-			
+
 			/* Read the file */
 			if ($strict) {
 				if (is_file($loadFile)) $this->Buffer = file_get_contents($loadFile);
@@ -69,67 +69,67 @@
 			}
 			return true;
 		}
-		
+
 		// LOAD SPECIFIC BUFFER CONTENT ///////////////////////////////////////////////
 		function LoadThis($content) {
 			global $CONF;
-			
+
 			$this->Buffer = $content;
 			if ($CONF["TRANSLATOR_ENABLED"]) $this->Translate();
 			return true;
 		}
-			
+
 		// ASSIGN ARRAY ///////////////////////////////////////////////////////////////
 		function AssignArray($array) {
 			foreach ($array as $code => $value) {
 				$this->Buffer = str_replace("{".$code."}", $value, $this->Buffer);
 			}
 		}
-		
+
 		// ASSIGN VALUE ///////////////////////////////////////////////////////////////
 		function AssignValue($tag, $value) {
 			$this->Buffer = str_replace("{".$tag."}", $value, $this->Buffer);
 		}
-		
+
 		// ASSIGNROW //////////////////////////////////////////////////////////////////
 		function AssignRow($prefix, $array) {
 			foreach ($array as $code => $value) {
 				$this->Buffer = str_replace("{".$prefix.".".$code."}", $value, $this->Buffer);
 			}
 		}
-				
+
 		// ASSIGN USER ////////////////////////////////////////////////////////////////
 		function AssignUser($user) {
 			if (strstr($this->Buffer, "{user.")) {
 				if (($user) && (!is_array($user))) {
-					$user = myF(myQ("
+					$user = myF("
 						SELECT *
 						FROM `[x]users`
 						WHERE `id`='{$user}'
 						LIMIT 1
-					"));
+					");
 				}
 				if (is_array($user)) {
 					foreach ($user as $code => $value) {
 						$this->Buffer = str_replace("{user.".$code."}", $value, $this->Buffer);
 					}
 				}
-			}		
+			}
 		}
-				
+
 		// ASSIGN LOOP ////////////////////////////////////////////////////////////////
 		function Loop($name, $array) {
 			global $CONF;
-			
+
 			preg_match("/\<LOOP {$name}\>(.*)\<\/LOOP {$name}\>/isU", $this->Buffer, $tag);
-			
+
 			if (isset($tag[1])) {
-				
+
 				$tag[1] = $this->StateZones($tag[1]);
-				
+
 				/* Initialize the final result container string */
 				$ret = NULL;
-				
+
 				foreach($array as $key => $arr) {
 					if (is_array($arr)) {
 						$temp = $tag[1];
@@ -139,7 +139,7 @@
 						$ret .= $temp;
 					}
 				}
-				
+
 				$this->Buffer = str_replace($tag[0], $ret, $this->Buffer);
 				return true;
 			}
@@ -147,7 +147,7 @@
 
 		// GET OBJECTS ////////////////////////////////////////////////////////////////
 		function GetObjects() {
-			/* 
+			/*
 				This will load all objects found in the template file into the
 				$GLOBALS["OBJ"] array. The array key ($GLOBALS["OBJ"][KEY]) goes
 				by the object name defined in the template.
@@ -155,7 +155,7 @@
 
 			preg_match_all("|\<OBJ ([^>]+)\>(.*)\</OBJ [^>]+\>|isU", $this->Buffer, $obj, PREG_SET_ORDER);
 
-			/* Match result: 
+			/* Match result:
 				Array[i][0] => Full object with surrounding tags
 				Array[i][1] => Object name
 				Array[i][2] => Full object WITHOUT surrounding tags
@@ -170,22 +170,22 @@
 				}
 			}
 		}
-		
+
 		// USE ZONE ///////////////////////////////////////////////////////////////////
 		function Zone($group, $zone, $retmode=false) {
-			/* 
+			/*
 				This function will swap zones by groups names and remove other zones
 				for the same group
 			*/
-			
+
 			preg_match_all("|\<ZONE {$group} ([^>]+)\>(.*)\<\/ZONE {$group} [^>]+\>|isU", $this->Buffer, $zn, PREG_SET_ORDER);
 
-			/* Match result: 
+			/* Match result:
 				Array[i][0] => Full zone with surrounding tags
 				Array[i][1] => zone name
 				Array[i][2] => Full zone WITHOUT surrounding tags
 			*/
-		
+
 			if (isset($zn) && is_array($zn)) {
 				foreach ($zn as $key => $znArray) {
 					if (is_array($znArray)) {
@@ -197,25 +197,25 @@
 						}
 					}
 				}
-			}		
+			}
 		}
 
 
 		// CLEAN ZONES ////////////////////////////////////////////////////////////////
 		function CleanZones() {
-			/* 
+			/*
 				This function will remove uncalled zones before the template
 				is flushed.
 			*/
-			
+
 			preg_match_all("|\<ZONE (.*)\>.*\<\/ZONE \\1\>|isU", $this->Buffer, $zn, PREG_SET_ORDER);
 
-			/* Match result: 
+			/* Match result:
 				Array[i][0] => Full zone with surrounding tags
 				Array[i][1] => zone name
 				Array[i][2] => Full zone WITHOUT surrounding tags
 			*/
-		
+
 			if (isset($zn) && is_array($zn)) {
 				foreach ($zn as $key => $znArray) {
 					if (is_array($znArray)) {
@@ -230,31 +230,31 @@
 			preg_match('%<select.*name="'.$group.'".*?>(.*)</select>%si', $this->Buffer, $groupMatch);
 			if (isset($groupMatch[1])) {
 				$this->Buffer = str_replace(
-					$groupMatch[1], 
+					$groupMatch[1],
 					preg_replace(
-						'/(<option[^<]?value="'.$option.'")([^>]?>)/si', 
-						'\\1 selected="selected"\\2', 
+						'/(<option[^<]?value="'.$option.'")([^>]?>)/si',
+						'\\1 selected="selected"\\2',
 						$groupMatch[1]
-					), 
+					),
 					$this->Buffer
 				);
 			}
 		}
-		
+
 		// CONVERT SELF USER //////////////////////////////////////////////////////////
 		function ConvertSelf() {
 			if (strstr($this->Buffer, "{me.")) {
 				if ((!isset($GLOBALS["SELF_USER_DATA"])) || (!$GLOBALS["SELF_USER_DATA"])) { me('id'); }
-				
+
 				if (isset($GLOBALS["SELF_USER_DATA"]) && is_array($GLOBALS["SELF_USER_DATA"])) {
-				
+
 					foreach ($GLOBALS["SELF_USER_DATA"] as $code => $value) {
 						$this->Buffer = str_replace("{me.".$code."}", $value, $this->Buffer);
 					}
 				}
 			}
 		}
-		
+
 		// TRANSLATE TEMPLATE /////////////////////////////////////////////////////////
 		function Translate() {
 			global $CONF;
@@ -268,7 +268,7 @@
 					$dictionaryFile = $CONF["LOCALE_LANGUAGEPACK_LOCATION"]."/".$CONF["LOCALE_SITE_DEFAULT_LANGUAGE"].".php";
 					$language = $CONF["LOCALE_SITE_DEFAULT_LANGUAGE"];
 				}
-	
+
 				if (strtolower($language) != "english" and !isset($GLOBALS['dictionary']) and is_file($dictionaryFile)) {
 					if (include ($dictionaryFile)) $translator_flat_mode = false;
 					else $translator_flat_mode = true;
@@ -279,17 +279,17 @@
 
 
 			$this->Buffer = preg_replace(
-				'/\\[([^\\]\\[]+) ?\\{(\\d+)\\}]/Usie',
-				($translator_flat_mode ? "'\\1';" : "\\\$GLOBALS['dictionary']['\\2']"), 
+				'/\\[([^\\]\\[]+) ?\\{(\\d+)\\}]/Usi',
+				($translator_flat_mode ? "'\\1';" : "\\\$GLOBALS['dictionary']['\\2']"),
 				$this->Buffer
 			);
 		}
-		
+
 		// USER STATES ////////////////////////////////////////////////////////////////
 		function StateZones($buffer) {
-		
-			/* 
-				Deal with is_* tags, we remove the tag and keep the 
+
+			/*
+				Deal with is_* tags, we remove the tag and keep the
 				content if true, remove tag and content if false.
 			*/
 			if (preg_match('/<is_op>|<is_mop>|<is_guest>|<is_user>/i', $buffer)) {
@@ -300,13 +300,13 @@
 			}
 			return $buffer;
 		}
-			
- 
+
+
 		// FLUSH TEMPLATE /////////////////////////////////////////////////////////////
 		function Flush($return=false) {
-			
+
 			$this->Buffer = $this->StateZones($this->Buffer);
-			
+
 			if (!$return) echo $this->Buffer;
 			else return $this->Buffer;
 		}
--- system/functions/database/mysql.fnc.php
+++ system/functions/database/mysql.fnc.php
@@ -5,12 +5,12 @@
 //                                                                                   //
 // Please read the LICENSE.md & README.md file before using/modifying this software  //
 //                                                                                   //
-// Developing Author:       Andrew James, RubberDucky - andy@phpizabi.org            //
-// Last modification date:  December 13th, 201                                       //
+// Developing Author:       Andy James, AndyWTBlueHair - andy@andy.blue              //
+// Last modification date:  April 7th, 2016                                          //
 // Version:                 PHPizabi 2.01 Alpha                                      //
 //                                                                                   //
 // (C) 2005, 2006 Real!ty Medias                                                     //
-// (C) 2007-2012 AndyJames.Org                                                       //
+// (C) 2007-2016 Andy.Blue                                                           //
 //                                                                                   //
 // PHPizabi Is the work of a very talented development team. This script is our      //
 // Pride and Joy. We hope that you enjoy using this software as much as we enjoy     //
@@ -18,39 +18,40 @@
 //                                                                                   //
 ///////////////////////////////////////////////////////////////////////////////////////
 
-	/* Check Structure Availability */
-	if (!defined("CORE_STRAP")) die("Out of structure call");
+/* Check Structure Availability */
+if (!defined("CORE_STRAP")) die("Out of structure call");
 
-	function myConnect() { // MYSQL Database connection //
-		global $CONF;
+function myConnect() { // MYSQL Database connection //
+	global $CONF;
 
-		$GLOBALS["MYSQL_CONNECTION"] = @mysql_connect(
-			$CONF["MYSQL_DATABASE_HOSTNAME"], 
-			$CONF["MYSQL_DATABASE_USERNAME"], 
-			$CONF["MYSQL_DATABASE_PASSWORD"]
-		);
-		
-		if (!@mysql_select_db($CONF["MYSQL_DATABASE_DATABASENAME"])) die(mysql_error());
-	}
-	
-	function myQ($content) {
-		global $CONF;
-		
-		if (!isset($GLOBALS["MYSQL_CONNECTION"]) or !$GLOBALS["MYSQL_CONNECTION"]) myConnect();
-		return mysql_query(str_replace("[x]", $CONF["MYSQL_DATABASE_TABLES_PREFIX"], $content)); 
-	}
-	
-	function myF($content) {
-		if (!isset($GLOBALS["MYSQL_CONNECTION"]) or !$GLOBALS["MYSQL_CONNECTION"]) myConnect();
-		return mysql_fetch_array($content);
-	}
-	
-	function myNum($content) {
-		if (!isset($GLOBALS["MYSQL_CONNECTION"]) or !$GLOBALS["MYSQL_CONNECTION"]) myConnect();
-		return mysql_num_rows($content);
-	}
-	
-	function myClose() {
-		return @mysql_close();
-	}
+	$GLOBALS["MYSQL_CONNECTION"] = new PDO("mysql:host={$CONF["MYSQL_DATABASE_HOSTNAME"]};dbname={$CONF["MYSQL_DATABASE_DATABASENAME"]}",
+	$CONF["MYSQL_DATABASE_USERNAME"],
+	$CONF["MYSQL_DATABASE_PASSWORD"]);
+
+}
+
+# This function needs the fetch removed from it
+function myQ($content) {
+	global $CONF;
+
+	if (!isset($GLOBALS["MYSQL_CONNECTION"]) or !$GLOBALS["MYSQL_CONNECTION"]) myConnect();
+	$GLOBALS["MYSQL_CONNECTION"]->query(str_replace("[x]", $CONF["MYSQL_DATABASE_TABLES_PREFIX"], $content));
+}
+
+function myF($content) {
+	global $CONF;
+
+	if (!isset($GLOBALS["MYSQL_CONNECTION"]) or !$GLOBALS["MYSQL_CONNECTION"]) myConnect();
+	return $GLOBALS["MYSQL_CONNECTION"]->query(str_replace("[x]", $CONF["MYSQL_DATABASE_TABLES_PREFIX"], $content))->fetch(PDO::FETCH_BOTH);
+}
+
+# This function needs re-written to provide the proper output
+function myNum($content) {
+	if (!isset($GLOBALS["MYSQL_CONNECTION"]) or !$GLOBALS["MYSQL_CONNECTION"]) myConnect();
+	return mysql_num_rows($content);
+}
+
+function myClose() {
+$GLOBALS["MYSQL_CONNECTION"] = null;
+}
 ?>
